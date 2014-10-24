8ba9a587f0fcaf51230ebd859480ca2903c4a38d
Ilham Laenur Hikmat <ilhamlaen@gmail.com>
2014-10-24 13:25:24 +0700

    Segmentasi dan normalisasi beres

--- main.cpp
+++ main.cpp
@@ -19,9 +19,10 @@ int main(int argc, char *argv[])
 
     cvtColor(im_src, im_grey, CV_BGR2GRAY);
     cvtColor(im_src, im_segmented, CV_BGR2GRAY);
-    cvtColor(im_src, im_normalized, CV_BGR2GRAY);
 
     //--------------Image Segmentation
+    int left_limit = im_grey.cols, right_limit = 0, top_limit = im_grey.rows, bottom_limit = 0;
+
     int segmentation_block_size = 16;
     for (int i = 0; i < im_grey.cols; i = i + segmentation_block_size)
     {
@@ -51,46 +52,58 @@ int main(int argc, char *argv[])
             {
                 for (int _j = 0; _j < segmentation_block_size; _j++)
                 {
-                    if (sub_var > 100) im_segmented.at<uchar>(Point(i+_i,j+_j)) = im_grey.at<uchar>(Point(i+_i,j+_j));
-                    else im_segmented.at<uchar>(Point(i+_i,j+_j)) = 0;
+                    if (sub_var > 100)
+                    {
+                        if (i+_i < left_limit) left_limit = i+_i;
+                        if (i+_i > right_limit) right_limit = i+_i;
+                        if (j+_j < top_limit) top_limit = j+_j;
+                        if (j+_j > bottom_limit) bottom_limit = j+_j;
+                    }
                 }
             }
         }
     }
+    im_segmented = im_grey(Rect(left_limit, top_limit, right_limit-left_limit,bottom_limit-top_limit));
 
     //--------------Image Normalization
     double mean = 0, variance = 0;
     double desired_mean = 0, desired_variance = 1;
 
-    for (int i = 0; i < im_grey.rows; i++)
+    im_normalized = Mat(im_segmented.rows, im_segmented.cols, CV_8UC1);
+
+    for (int i = 0; i < im_segmented.rows; i++)
     {
-        for (int j = 0; j < im_grey.cols; j++)
+        for (int j = 0; j < im_segmented.cols; j++)
         {
             mean = mean + im_segmented.at<uchar>(Point(i, j));
+            printf("cur:%d sum:%f\n", im_segmented.at<uchar>(Point(i, j)), mean);
         }
     }
     mean = mean / (im_segmented.rows*im_segmented.cols);
+    printf("mean:%f var:%f num:%d\n", mean, variance, im_segmented.rows*im_segmented.cols);
 
-    for (int i = 0; i < im_grey.rows; i++)
+    for (int i = 0; i < im_segmented.rows; i++)
     {
-        for (int j = 0; j < im_grey.cols; j++)
+        for (int j = 0; j < im_segmented.cols; j++)
         {
             variance = variance + pow(im_segmented.at<uchar>(Point(i, j)) - mean, 2);
         }
     }
-    mean = mean / (im_segmented.rows*im_segmented.cols);
+    variance = variance / (im_segmented.rows*im_segmented.cols);
 
-    for (int j = 0; j < im_grey.rows; j++)
+    for (int j = 0; j < im_normalized.rows; j++)
     {
-        for (int i = 0; i < im_grey.cols; i++)
+        for (int i = 0; i < im_normalized.cols; i++)
         {
             if (im_segmented.at<uchar>(Point(i, j)) > mean) im_normalized.at<uchar>(Point(i, j)) = desired_mean + pow((desired_variance*pow(im_segmented.at<uchar>(Point(i, j)) - mean, 2))/variance, 0.5);
             else im_normalized.at<uchar>(Point(i, j)) = desired_mean - pow((desired_variance*pow(im_segmented.at<uchar>(Point(i, j)) - mean, 2))/variance, 0.5);
         }
     }
 
+    //---------------Orientation Estimation
+
     imshow( "src", im_segmented );
-    imshow( "segmented", im_normalized );
+    imshow( "segmented", im_normalized);
 
     waitKey(0);
     return 0;
